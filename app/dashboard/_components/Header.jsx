3575c172fe89f5ff93ef4ad1ea99de5537371171
Nikhil <harshita10203040@gmail.com>
2024-07-03 10:53:45 +0530

    first commit

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -17,37 +17,47 @@ const Header = () => {
   };
 
   return (
-    <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-      <Image src={'/logo.svg'} width={160} height={160}/>
-      <ul className='hidden md:flex gap-6'>
-        <li 
-          className={`hover:text-primary hover:font-bold translate-x-0 cursor-pointer ${path === '/dashboard' ? 'text-primary font-bold' : ''}`}
-          onClick={() => handleNavigation('/dashboard')}
+    <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
+      <a href="http://localhost:3000/">
+        <Image src="/logo.svg" width={160} height={160} alt="Logo" />
+      </a>
+      <ul className="hidden md:flex gap-6">
+        <li
+          className={`hover:text-primary hover:font-bold translate-x-0 cursor-pointer ${
+            path === "/dashboard" ? "text-primary font-bold" : ""
+          }`}
+          onClick={() => handleNavigation("/dashboard")}
         >
           DashBoard
         </li>
-        <li 
-          className={`hover:text-primary hover:font-bold translate-x-0 cursor-pointer ${path === '/dashboard/questions' ? 'text-primary font-bold' : ''}`}
-          onClick={() => handleNavigation('/dashboard/questions')}
+        <li
+          className={`hover:text-primary hover:font-bold translate-x-0 cursor-pointer ${
+            path === "/dashboard/questions" ? "text-primary font-bold" : ""
+          }`}
+          onClick={() => handleNavigation("/dashboard/questions")}
         >
           Questions
         </li>
-        <li 
-          className={`hover:text-primary hover:font-bold translate-x-0 cursor-pointer ${path === '/dashboard/upgrade' ? 'text-primary font-bold' : ''}`}
-          onClick={() => handleNavigation('/dashboard/upgrade')}
+        <li
+          className={`hover:text-primary hover:font-bold translate-x-0 cursor-pointer ${
+            path === "/dashboard/upgrade" ? "text-primary font-bold" : ""
+          }`}
+          onClick={() => handleNavigation("/dashboard/upgrade")}
         >
           Upgrade
         </li>
-        <li 
-          className={`hover:text-primary hover:font-bold translate-x-0 cursor-pointer ${path === '/dashboard/faq' ? 'text-primary font-bold' : ''}`}
-          onClick={() => handleNavigation('/dashboard/faq')}
+        <li
+          className={`hover:text-primary hover:font-bold translate-x-0 cursor-pointer ${
+            path === "/dashboard/faq" ? "text-primary font-bold" : ""
+          }`}
+          onClick={() => handleNavigation("/dashboard/faq")}
         >
           How it works?
         </li>
       </ul>
-      <UserButton/>
+      <UserButton />
     </div>
-  )
+  );
 }
 
 export default Header
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,22 +1,93 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
-import AddNewInterview from './_components/AddNewInterview'
-import InterviewList from './_components/InterviewList'
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+import "./styles.css";
 
 const Dashboard = () => {
+  const router = useRouter();
+  const handleNavigation = (route) => {
+    router.push(route);
+  };
+
   return (
-    <div className='p-10'>
-      <h2 className='font-bold text-2xl'>DashBoard</h2>
-      <h2 className='capitalize text-grey-500'>create and start your AI mock up interview</h2>
-
-      <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
-        <AddNewInterview />
-
+    <div className="p-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 my-5 p-10 gap-8">
+        <Link href={"http://localhost:3002/"}>
+          <div className="border shadow-sm rounded-lg p-3">
+            <h2 className="font-bold text-primary capitalize text-2xl">
+              Video Conferencing
+            </h2>
+            <div className="image-container">
+              <img
+                src="vc.jpg"
+                alt=""
+                srcSet=""
+                className="h-100 w-full mt-5 rounded-lg hover-zoom"
+              />
+              <div className="image-overlay">
+                <p className="overlay-text">Click to join a video conference</p>
+              </div>
+            </div>
+          </div>
+        </Link>
+        <Link href={"http://localhost:5173"}>
+          <div className="border shadow-sm rounded-lg p-3">
+            <h2 className="font-bold text-white capitalize text-2xl">IDE</h2>
+            <div className="image-container">
+              <img
+                src="ide.jpg"
+                alt=""
+                srcSet=""
+                className="h-100 w-full mt-5 border rounded-lg hover-zoom"
+              />
+              <div className="image-overlay">
+                <p className="overlay-text">
+                  Edit and run HTML, CSS, and JavaScript
+                </p>
+              </div>
+            </div>
+          </div>
+        </Link>
+        <a href={"http://localhost:3001/"} className="link-wrapper">
+          <div className="border shadow-sm rounded-lg p-3">
+            <h2 className="font-bold text-primary capitalize text-2xl">
+              ChatBot
+            </h2>
+            <div className="image-container">
+              <img
+                src="chat.jpg"
+                alt="ChatBot"
+                className="chatbot-image hover-zoom"
+              />
+              <div className="image-overlay">
+                <p className="overlay-text">Real-time chat application</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <Link href={"http://localhost:3001/"}>
+          <div className="border shadow-sm rounded-lg p-3">
+            <h2 className="font-bold text-white capitalize text-2xl">
+              Text Sharing
+            </h2>
+            <div className="image-container">
+              <img
+                src="chatb.jpg"
+                alt=""
+                srcSet=""
+                className="h-100 w-full mt-5 border rounded-lg hover-zoom"
+              />
+              <div className="image-overlay">
+                <p className="overlay-text">Share code live</p>
+              </div>
+            </div>
+          </div>
+        </Link>
       </div>
-
-      <InterviewList/>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
